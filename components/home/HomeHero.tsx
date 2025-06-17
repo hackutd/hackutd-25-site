7ab16c754e36a4a8e0979ec8d25f54dc6d336704
Rayyan Waris <raywa04@gmail.com>
2025-06-17 14:54:43 -0500

    feat: Implement blob page

--- components/home/HomeHero.tsx
+++ components/home/HomeHero.tsx
@@ -1,20 +1,35 @@
-import BackgroundCircles from '../BackgroundCircles';
-import AppHeader from '../AppHeader';
+import Image from 'next/image';
+import MLH_Sticker from '../../public/assets/mlh-sticker.png';
+//import HackTitle from './HackTitle';
+import { useState } from 'react';
+import { RequestHelper } from '../../lib/request-helper';
 
 export default function HomeHero() {
+  const [userEmail, setUserEmail] = useState<string>('');
+  const handleSubmitEmail = async (userEmail: string) => {
+    const res = await RequestHelper.post<{ userEmail: string }, unknown>(
+      '/api/email',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+      {
+        userEmail,
+      },
+    );
+    if (res.status === 200) {
+      alert('Email submitted successful');
+    } else {
+      alert('Something is wrong... please try again later');
+    }
+  };
   return (
-    <section className="min-h-screen bg-contain bg-white flex flex-col-reverse md:flex-col">
-      {/* App header */}
-      <AppHeader />
-
+    <section className="min-h-screen bg-cover bg-hero-pattern bg-no-repeat bg-center flex flex-col-reverse md:flex-col">
       <div className="flex h-screen w-full relative">
-        <div className="w-full h-full absolute top-0 left-0 z-0">
-          <BackgroundCircles />
-        </div>
-
         <div className="relative z-10 shrink-0 w-full flex">
           {/* MLH sticker */}
-          {/* <div className="absolute top-0 right-4 z-20">
+          <div className="absolute top-0 right-4 z-20">
             <Image
               src={MLH_Sticker.src}
               height={MLH_Sticker.height}
@@ -22,25 +37,83 @@ export default function HomeHero() {
               alt="MLH sticker"
               className="w-full h-full object-cover"
             />
-          </div> */}
+          </div>
 
           {/* Big welcome */}
-          <div className="w-full flex flex-col gap-2 justify-center items-center bg-[rgba(255,255,255,0.75)] backdrop-blur-[60px]">
-            <p className="font-nunito text-[#262626] text-xl md:text-3xl">Welcome To</p>
-            <h1 className="font-fredokaOne text-4xl md:text-6xl lg:text-8xl font-bold text-[#05149C]">
-              HACKPORTAL
-            </h1>
+          <div className="w-full flex flex-col gap-2 justify-center items-center">
+            {/* <HackTitle /> */}
+            <p className="font-poppins text-white font-medium">
+              Get notified when application drops
+            </p>
+            <div className="rounded-xl border-none w-2/5 p-[5px] flex items-center bg-white gap-x-3">
+              <input
+                type="text"
+                className="w-4/5 rounded-lg border-none focus:ring-0"
+                placeholder="Email Address"
+                value={userEmail}
+                onChange={(e) => setUserEmail(e.target.value)}
+              />
+              <button
+                className="rounded-lg bg-[#F7CE79] px-4 py-3 w-1/5 text-white"
+                onClick={async () => {
+                  await handleSubmitEmail(userEmail);
+                }}
+              >
+                Submit
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-
-      {/* Bottom banner */}
-      <div className="font-dmSans w-full flex justify-center bg-[#7B81FF] text-white h-[1.75rem] text-nowrap overflow-hidden">
-        <p className="text-lg">
-          SAMPLE TEXT • SAMPLE TEXT • SAMPLE TEXT • SAMPLE TEXT • SAMPLE TEXT • SAMPLE TEXT • SAMPLE
-          TEXT • SAMPLE TEXT • SAMPLE TEXT
-        </p>
       </div>
     </section>
   );
 }
+
+//Bring this back later
+
+// import BackgroundCircles from '../BackgroundCircles';
+// import AppHeader from '../AppHeader';
+
+// export default function HomeHero() {
+//   return (
+//     <section className="min-h-screen bg-contain bg-white flex flex-col-reverse md:flex-col">
+//       {/* App header */}
+//       <AppHeader />
+
+//       <div className="flex h-screen w-full relative">
+//         <div className="w-full h-full absolute top-0 left-0 z-0">
+//           <BackgroundCircles />
+//         </div>
+
+//         <div className="relative z-10 shrink-0 w-full flex">
+//           {/* MLH sticker */}
+//           {/* <div className="absolute top-0 right-4 z-20">
+//             <Image
+//               src={MLH_Sticker.src}
+//               height={MLH_Sticker.height}
+//               width={MLH_Sticker.width}
+//               alt="MLH sticker"
+//               className="w-full h-full object-cover"
+//             />
+//           </div> */}
+
+//           {/* Big welcome */}
+//           <div className="w-full flex flex-col gap-2 justify-center items-center bg-[rgba(255,255,255,0.75)] backdrop-blur-[60px]">
+//             <p className="font-nunito text-[#262626] text-xl md:text-3xl">Welcome To</p>
+//             <h1 className="font-fredokaOne text-4xl md:text-6xl lg:text-8xl font-bold text-[#05149C]">
+//               HACKPORTAL
+//             </h1>
+//           </div>
+//         </div>
+//       </div>
+
+//       {/* Bottom banner */}
+//       <div className="font-dmSans w-full flex justify-center bg-[#7B81FF] text-white h-[1.75rem] text-nowrap overflow-hidden">
+//         <p className="text-lg">
+//           SAMPLE TEXT • SAMPLE TEXT • SAMPLE TEXT • SAMPLE TEXT • SAMPLE TEXT • SAMPLE TEXT • SAMPLE
+//           TEXT • SAMPLE TEXT • SAMPLE TEXT
+//         </p>
+//       </div>
+//     </section>
+//   );
+// }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,28 @@
+import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import { useEffect, useState } from 'react';
-import Head from 'next/head';
+import { RequestHelper } from '../lib/request-helper';
+import HomeHero from '../components/home/HomeHero';
 
-import HomeChallengesComponent from '@/components/home/challenge';
-import HomeHero from '@/components/home/HomeHero';
-import HackUTDCountdown from '@/components/home/countdown';
-import HomeAboutText from '@/components/home/about/HomeAboutText';
-import HomeAboutPhotos from '@/components/home/about/HomeAboutPhotos';
-import HomeSchedule from '@/components/home/HomeSchedule';
-import HomeFaq from '@/components/home/faq';
-import HomeSponsors from '@/components/home/sponsors';
-import HomeFooter from '@/components/home/HomeFooter';
-
-import { RequestHelper } from '@/lib/request-helper';
-
-interface Props {
+/**
+ * The home page.
+ *
+ * Landing: /
+ *
+ */
+export default function Home(props: {
+  keynoteSpeakers: KeynoteSpeaker[];
+  challenges: Challenge[];
   answeredQuestion: AnsweredQuestion[];
+  fetchedMembers: TeamMember[];
   sponsorCard: Sponsor[];
   scheduleCard: ScheduleEvent[];
   dateCard: Dates;
-  challenges: Challenge[];
-}
-
-export default function Home({
-  answeredQuestion,
-  challenges,
-  dateCard,
-  scheduleCard,
-  sponsorCard,
-}: Props) {
+  prizeData: Array<{ rank: number; prizeName: string }>;
+}) {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
+    // Wait for all components to render before showing page
     setLoading(false);
   }, []);
 
@@ -45,54 +37,172 @@ export default function Home({
   return (
     <>
       <Head>
-        <title>HackPortal</title>
-        <meta name="description" content="A default HackPortal instance" />
+        <title>HackUTD 2025</title> {/* !change */}
+        <meta name="description" content="HackUTD 2025 Event Site" /> {/* !change */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="overflow-x-hidden w-full">
-        <HomeHero />
-        <HackUTDCountdown />
-        <HomeAboutText />
-        <HomeAboutPhotos />
-        <HomeSchedule scheduleCard={scheduleCard} dateCard={dateCard} />
-        <HomeChallengesComponent challenges={challenges} />
-        <HomeFaq answeredQuestions={answeredQuestion} />
-        <HomeSponsors />
-        <HomeFooter />
-      </div>
+      <HomeHero />
+      {/* <HomeNotif />
+      <HomeHero />
+      <HomeVideoStats />
+      <HackCountdown />
+      <HomeAbout />
+      <HomeSchedule scheduleCard={props.scheduleCard} dateCard={props.dateCard} />
+      <HomeSpeakers keynoteSpeakers={props.keynoteSpeakers} />
+      <HomeChallenges challenges={props.challenges} />
+      <HomePrizes prizes={props.prizeData} />
+      <HomeTeam members={props.fetchedMembers} />
+      <HomeFaq answeredQuestion={props.answeredQuestion} />
+      <HomeSponsors sponsorCard={props.sponsorCard} />
+      <HomeFooter /> */}
     </>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const protocol = context.req.headers.referer?.split('://')[0] || 'http';
-  const { data: keynoteData } = await RequestHelper.get<KeynoteSpeaker[]>(
-    `${protocol}://${context.req.headers.host}/api/keynotespeakers`,
-    {},
-  );
-  const { data: challengeData } = await RequestHelper.get<Challenge[]>(
-    `${protocol}://${context.req.headers.host}/api/challenges/`,
-    {},
-  );
-  const { data: answeredQuestion } = await RequestHelper.get<AnsweredQuestion[]>(
-    `${protocol}://${context.req.headers.host}/api/questions/faq`,
-    {},
-  );
-  const { data: scheduleData } = await RequestHelper.get<ScheduleEvent[]>(
-    `${protocol}://${context.req.headers.host}/api/schedule`,
-    {},
-  );
-  const { data: dateData } = await RequestHelper.get<ScheduleEvent[]>(
-    `${protocol}://${context.req.headers.host}/api/dates`,
-    {},
-  );
-  return {
-    props: {
-      keynoteSpeakers: keynoteData,
-      challenges: challengeData,
-      answeredQuestion: answeredQuestion,
-      scheduleCard: scheduleData,
-      dateCard: dateData,
-    },
-  };
-};
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const protocol = context.req.headers.referer?.split('://')[0] || 'http';
+//   const { data: keynoteData } = await RequestHelper.get<KeynoteSpeaker[]>(
+//     `${protocol}://${context.req.headers.host}/api/keynotespeakers`,
+//     {},
+//   );
+//   const { data: challengeData } = await RequestHelper.get<Challenge[]>(
+//     `${protocol}://${context.req.headers.host}/api/challenges/`,
+//     {},
+//   );
+//   const { data: prizeData } = await RequestHelper.get<Array<{ rank: number; prizeName: string }>>(
+//     `${protocol}://${context.req.headers.host}/api/prizes`,
+//     {},
+//   );
+//   const { data: answeredQuestion } = await RequestHelper.get<AnsweredQuestion[]>(
+//     `${protocol}://${context.req.headers.host}/api/questions/faq`,
+//     {},
+//   );
+//   const { data: memberData } = await RequestHelper.get<TeamMember[]>(
+//     `${protocol}://${context.req.headers.host}/api/members`,
+//     {},
+//   );
+//   const { data: sponsorData } = await RequestHelper.get<Sponsor[]>(
+//     `${protocol}://${context.req.headers.host}/api/sponsor`,
+//     {},
+//   );
+//   const { data: scheduleData } = await RequestHelper.get<ScheduleEvent[]>(
+//     `${protocol}://${context.req.headers.host}/api/schedule`,
+//     {},
+//   );
+//   const { data: dateData } = await RequestHelper.get<ScheduleEvent[]>(
+//     `${protocol}://${context.req.headers.host}/api/dates`,
+//     {},
+//   );
+//   return {
+//     props: {
+//       keynoteSpeakers: keynoteData,
+//       challenges: challengeData,
+//       answeredQuestion: answeredQuestion,
+//       fetchedMembers: memberData,
+//       sponsorCard: sponsorData,
+//       scheduleCard: scheduleData,
+//       dateCard: dateData,
+//       prizeData: prizeData,
+//     },
+//   };
+// };
+
+// Add this back later
+// import { GetServerSideProps } from 'next';
+// import { useEffect, useState } from 'react';
+// import Head from 'next/head';
+
+// import HomeChallengesComponent from '@/components/home/challenge';
+// import HomeHero from '@/components/home/HomeHero';
+// import HackUTDCountdown from '@/components/home/countdown';
+// import HomeAboutText from '@/components/home/about/HomeAboutText';
+// import HomeAboutPhotos from '@/components/home/about/HomeAboutPhotos';
+// import HomeSchedule from '@/components/home/HomeSchedule';
+// import HomeFaq from '@/components/home/faq';
+// import HomeSponsors from '@/components/home/sponsors';
+// import HomeFooter from '@/components/home/HomeFooter';
+
+// import { RequestHelper } from '@/lib/request-helper';
+
+// interface Props {
+//   answeredQuestion: AnsweredQuestion[];
+//   sponsorCard: Sponsor[];
+//   scheduleCard: ScheduleEvent[];
+//   dateCard: Dates;
+//   challenges: Challenge[];
+// }
+
+// export default function Home({
+//   answeredQuestion,
+//   challenges,
+//   dateCard,
+//   scheduleCard,
+//   sponsorCard,
+// }: Props) {
+//   const [loading, setLoading] = useState(true);
+//   useEffect(() => {
+//     setLoading(false);
+//   }, []);
+
+//   if (loading) {
+//     return (
+//       <div>
+//         <h1>Loading...</h1>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <>
+//       <Head>
+//         <title>HackUTD 2025</title>
+//         <meta name="description" content="HackUTD 2025 Event Site" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+//       <div className="overflow-x-hidden w-full">
+//         <HomeHero />
+//         <HackUTDCountdown />
+//         <HomeAboutText />
+//         <HomeAboutPhotos />
+//         <HomeSchedule scheduleCard={scheduleCard} dateCard={dateCard} />
+//         <HomeChallengesComponent challenges={challenges} />
+//         <HomeFaq answeredQuestions={answeredQuestion} />
+//         <HomeSponsors />
+//         <HomeFooter />
+//       </div>
+//     </>
+//   );
+// }
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const protocol = context.req.headers.referer?.split('://')[0] || 'http';
+//   const { data: keynoteData } = await RequestHelper.get<KeynoteSpeaker[]>(
+//     `${protocol}://${context.req.headers.host}/api/keynotespeakers`,
+//     {},
+//   );
+//   const { data: challengeData } = await RequestHelper.get<Challenge[]>(
+//     `${protocol}://${context.req.headers.host}/api/challenges/`,
+//     {},
+//   );
+//   const { data: answeredQuestion } = await RequestHelper.get<AnsweredQuestion[]>(
+//     `${protocol}://${context.req.headers.host}/api/questions/faq`,
+//     {},
+//   );
+//   const { data: scheduleData } = await RequestHelper.get<ScheduleEvent[]>(
+//     `${protocol}://${context.req.headers.host}/api/schedule`,
+//     {},
+//   );
+//   const { data: dateData } = await RequestHelper.get<ScheduleEvent[]>(
+//     `${protocol}://${context.req.headers.host}/api/dates`,
+//     {},
+//   );
+//   return {
+//     props: {
+//       keynoteSpeakers: keynoteData,
+//       challenges: challengeData,
+//       answeredQuestion: answeredQuestion,
+//       scheduleCard: scheduleData,
+//       dateCard: dateData,
+//     },
+//   };
+// };
