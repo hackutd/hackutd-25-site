dbce795a05298957f9d509480e202788ce334de9
unknown <zhoukelly61@gmailk.com>
2025-06-19 23:31:29 -0500

    style: Banner

--- components/home/HomeHero.tsx
+++ components/home/HomeHero.tsx
@@ -35,8 +35,8 @@ export default function HomeHero() {
           <div className="absolute top-0 right-4 z-20">
             <Image
               src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-black.svg"
-              height={30}
-              width={80}
+              height={MLH_Sticker.height}
+              width={MLH_Sticker.width / 7}
               alt="MLH sticker"
               className="object-cover"
             />
